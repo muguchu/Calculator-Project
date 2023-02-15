4f5851d72501923b66f2fc8107ac0c2fcd84ab8a
muguchu titus <titusmuguchu@gmail.com>
2023-02-15 11:41:06 +0300

    added calculator functionf

--- calculator.py
+++ calculator.py
@@ -7,17 +7,29 @@ operator=input("Enter operator:")
 
 def add(number1,number2):
     result=number1 + number2
-    return result
+    print(result)
 
 def subtract(number1,number2):
     result = number1 - number2
-    return result
+    print(result)
 
 def divide(number1,number2):
     result=(number1/number2)
-    return result
+    print(result)
+
 
 def multiply(number1,number2):
     result=(number1*number2)
-    return result
+    print(result)
 
+if operator=="+":
+    add(number1,number2)
+elif operator=="-":
+    subtract(number1,number2)
+elif operator=="/":
+    divide(number1,number2)
+elif operator=="*" or operator=="X" or operator=="x":
+    multiply(number1,number2)
+else:
+    print("Invalid operator")
+    
